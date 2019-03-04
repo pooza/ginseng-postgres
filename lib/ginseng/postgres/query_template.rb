574ae94193cd3a5172fdb5cf7ef584114e982a08
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-03-04 10:18:07 +0900

    #10

---
+++ lib/ginseng/postgres/query_template.rb
@@ -0,0 +1,38 @@
+module Ginseng
+  module Postgres
+    class QueryTemplate < Ginseng::Template
+      include Package
+
+      def initialize(name)
+        name += '.sql' unless name =~ /\.sql$/
+        super(name)
+      end
+
+      def params=(params)
+        params.each do |k, v|
+          params[k] = db.escape_string(v) if v.is_a?(String)
+        end
+        super(params)
+      end
+
+      def []=(key, value)
+        value = db.escape_string(value) if value.is_a?(String)
+        super(key, value)
+      end
+
+      def to_s
+        return super.gsub(/\s+/, ' ').strip
+      end
+
+      private
+
+      def db
+        return Database.instance
+      end
+
+      def dir
+        return 'query'
+      end
+    end
+  end
+end
---
+++ test/query_template_test.rb
@@ -0,0 +1,32 @@
+module Ginseng
+  module Postgres
+    class QueryTemplateTest < Test::Unit::TestCase
+      def setup
+        @template = QueryTemplate.new('tables')
+      end
+
+      def test_new
+        assert(@template.is_a?(QueryTemplate))
+      end
+
+      def test_param
+        @template['schema'] = 'fuga'
+        assert_equal(@template.params[:schema], 'fuga')
+        @template[:schema] = 'hogehoge'
+        assert_equal(@template.params[:schema], 'hogehoge')
+      end
+
+      def test_params
+        @template.params = {'schema' => 'hoge'}
+        assert_equal(@template.params[:schema], 'hoge')
+        @template.params = {schema: 'information_schema'}
+        assert_equal(@template.params[:schema], 'information_schema')
+      end
+
+      def test_to_s
+        @template.params = {schema: 'information_schema'}
+        assert_equal(@template.to_s, %(SELECT * FROM information_schema.tables WHERE table_schema='information_schema'))
+      end
+    end
+  end
+end
