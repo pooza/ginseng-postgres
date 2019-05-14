76586e4d55768ed379e22d92e6cdea5a9039f2e9
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-05-15 08:27:35 +0900

    #20

--- lib/ginseng/postgres/database.rb
+++ lib/ginseng/postgres/database.rb
@@ -9,8 +9,8 @@ module Ginseng
       include Package
 
       def initialize
-        @config = config_class.constantize.instance
-        dsn = database_class.constantize.dsn
+        @config = config_class.instance
+        dsn = database_class.dsn
         dsn.dbname ||= default_dbname
         raise Ginseng::DatabaseError, 'Invalid DSN' unless dsn.absolute?
         raise Ginseng::DatabaseError, 'Invalid scheme' unless dsn.scheme == 'postgres'
@@ -28,9 +28,27 @@ module Ginseng
       end
 
       def create_sql(name, params = {})
-        template = query_template_class.constantize.new(name)
+        template = query_template_class.new(name)
         template.params = params
         return template.to_s
+      end
+
+      def begin
+        @db.exec('BEGIN')
+      rescue PG::Error => e
+        raise Ginseng::DatabaseError, e.message
+      end
+
+      def rollback
+        @db.exec('ROLLBACK')
+      rescue PG::Error => e
+        raise Ginseng::DatabaseError, e.message
+      end
+
+      def commit
+        @db.exec('COMMIT')
+      rescue PG::Error => e
+        raise Ginseng::DatabaseError, e.message
       end
 
       def execute(name, params = {})
