d240c05483d28e8273d6520881219cff93398c81
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-07-07 14:47:24 +0900

    ginseng-core 1.4.3

--- Rakefile
+++ Rakefile
@@ -6,9 +6,6 @@ require 'bundler/setup'
 require 'ginseng'
 require 'ginseng/postgres'
 
-desc 'test all'
-task test: 'ginseng:postgres:test'
-
 Dir.glob(File.join(Ginseng::Postgres::Environment.dir, 'lib/task/*.rb')).each do |f|
   require f
 end
--- lib/task/test.rb
+++ lib/task/test.rb
@@ -1,11 +1,8 @@
-namespace :ginseng do
-  namespace :postgres do
-    task :test do
-      ENV['TEST'] = Ginseng::Postgres::Package.name
-      require 'test/unit'
-      Dir.glob(File.join(Ginseng::Postgres::Environment.dir, 'test/*')).each do |t|
-        require t
-      end
-    end
+desc 'test all'
+task :test do
+  ENV['TEST'] = Ginseng::Postgres::Package.name
+  require 'test/unit'
+  Dir.glob(File.join(Ginseng::Postgres::Environment.dir, 'test/*')).each do |t|
+    require t
   end
 end
