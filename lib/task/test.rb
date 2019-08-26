94c42e554561a38a2516d45d97844a45950e582e
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-08-27 06:29:14 +0900

    #29

--- lib/task/test.rb
+++ lib/task/test.rb
@@ -2,7 +2,7 @@ desc 'test all'
 task :test do
   ENV['TEST'] = Ginseng::Postgres::Package.name
   require 'test/unit'
-  Dir.glob(File.join(Ginseng::Postgres::Environment.dir, 'test/*')).each do |t|
+  Dir.glob(File.join(Ginseng::Postgres::Environment.dir, 'test/*.rb')).each do |t|
     require t
   end
 end
